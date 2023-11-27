a5527937c957f428450d0505a1668e7c54128953
Haxwell <thehaxwell@proton.me>
2023-11-27 05:25:14 +0200

    prepared to download app on visit to /getting-started

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import svelte from "@astrojs/svelte";
 
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://www.joytyping.com',
-  integrations: [
-    starlight({
-      title: 'Joytyping',
-    }),
-  ],
+  integrations: [starlight({
+    title: 'Joytyping'
+  }), svelte()]
 });
